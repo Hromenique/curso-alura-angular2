8e00893cac9008e47f0fec974494a28ad1b6e647
Hromenique Batista <hromenique.batista@maplink.global>
2018-03-19 14:59:52 -0300

    final curso angular 2

--- client/app/listagem/listagem.component.ts
+++ client/app/listagem/listagem.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FotoComponent } from '../foto/foto.component';
 import { FotoService } from '../foto/foto.service';
+import { PainelComponent } from '../painel/painel.component';
 
 @Component({
     moduleId: module.id,
@@ -22,22 +23,26 @@ export class ListagemComponent {
             );
     }
 
-    remove(foto: FotoComponent): void {
+    remove(foto: FotoComponent, painel: PainelComponent): void {
         this.service
             .remove(foto)
             .subscribe(
             () => {
-                //mudar a referência para this.fotos para desencadear a detecção de mudança na view               
-                let novasFotos = this.fotos.slice(0);
-                let indice = novasFotos.indexOf(foto);
-                novasFotos.splice(indice, 1);
-                this.fotos = novasFotos;
-                this.mensagem = 'Foto removida com sucesso';
+
+                painel.fadeOut(() => {
+                    //mudar a referência para this.fotos para desencadear a detecção de mudança na view               
+                    let novasFotos = this.fotos.slice(0);
+                    let indice = novasFotos.indexOf(foto);
+                    novasFotos.splice(indice, 1);
+                    this.fotos = novasFotos;
+                    this.mensagem = 'Foto removida com sucesso';
+                });
+
             },
             erro => {
                 console.log(erro);
                 this.mensagem = 'Não foi possível remover a foto';
             }
-            );
+        );
     }
 }
